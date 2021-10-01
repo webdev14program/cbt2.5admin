c6f3dfd2716b9d418641433d04011d865d82a825
Rahmadika S Setiawan <dikakewren@outlook.com>
2021-10-01 19:18:45 +0700

    dashboard admin

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,6 +6,9 @@ class Dashboard extends CI_Controller
 
     public function index()
     {
-        $this->load->view('tampilan_dashboard');
+        $isi['content'] = 'tampilan_home';
+        $this->load->view('templates/header');
+        $this->load->view('tampilan_dashboard', $isi);
+        $this->load->view('templates/footer');
     }
 }
---
+++ application/views/tampilan_home.php
@@ -0,0 +1,36 @@
+<div class="card">
+    <div class="card-body bg-primary">
+        <h4 class="text-white text-uppercase font-weight-bold">cbt 2.5 smk tunas harapan</h4>
+    </div>
+</div>
+
+<div class="row mt-3">
+    <div class="col md">
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    </div>
+    <div class="col md">
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    </div>
+    <div class="col md">
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    </div>
+    <div class="col md">
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    </div>
+</div>
